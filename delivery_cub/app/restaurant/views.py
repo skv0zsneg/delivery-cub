0ce4eac8be620c702ac1af14d30b0454db7b04a4
skv0zsneg <klimentlamonov@yandex.ru>
2024-08-29 01:02:34 +0300

    Add filtering to Restaurant API.

--- delivery_cub/app/restaurant/views.py
+++ delivery_cub/app/restaurant/views.py
@@ -1,12 +1,34 @@
+from django_filters import rest_framework as filters
 from rest_framework import permissions, viewsets
 
-from app.restaurant.models import Restaurant, Dish
-from app.restaurant.serializers import RestaurantSerializer, DishSerializer
+from app.restaurant.models import Dish, Restaurant
+from app.restaurant.serializers import DishSerializer, RestaurantSerializer
 
+# Filters
+
+class RestaurantFilter(filters.FilterSet):
+    id = filters.NumberFilter(
+        label="ID ресторана",
+        lookup_expr="exact",
+    )
+    dish_name = filters.CharFilter(
+        label="Название блюда",
+        field_name="dishes__name",
+        lookup_expr="icontains",
+    )
+
+    class Meta:
+        model = Restaurant
+        fields = []
+
+
+# ViewSets
 
 class RestaurantViewSet(viewsets.ModelViewSet):
     queryset = Restaurant.objects.all()
     serializer_class = RestaurantSerializer
+    filter_backends = [filters.DjangoFilterBackend]
+    filterset_class = RestaurantFilter
     permission_classes = [permissions.IsAuthenticated]
 
 
--- delivery_cub/delivery_cub/settings.py
+++ delivery_cub/delivery_cub/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     "app.user",
 
     "rest_framework",
+    "django_filters",
     "drf_spectacular",
 ]
 
@@ -46,6 +47,9 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
+    ],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
