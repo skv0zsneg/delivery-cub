6d0b8cc62bd67300988aa103fd97e5845c6c83e5
skv0zsneg <klimentlamonov@yandex.ru>
2024-12-01 00:39:15 +0300

    Add adding to cart endpoint.

--- delivery_cub/app/custom_user/serializers.py
+++ delivery_cub/app/custom_user/serializers.py
@@ -1,6 +1,9 @@
+from uuid import UUID
+
 from rest_framework import serializers
 
 from app.custom_user.models import CartPosition, CustomUser
+from app.restaurant.models import Dish
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -13,7 +16,6 @@ class UserSerializer(serializers.ModelSerializer):
             "first_name",
             "last_name",
             "email",
-            "password",
         )
 
 
@@ -22,3 +24,20 @@ class CartPositionSerializer(serializers.ModelSerializer):
     class Meta:
         model = CartPosition
         fields = "__all__"
+
+
+class AddDishToCartSerializer(serializers.Serializer):
+    dish_id = serializers.UUIDField(write_only=True)
+    quantity = serializers.IntegerField(write_only=True)
+
+    def validate_quantity(self, value: int):
+        if value <= 0:
+            raise serializers.ValidationError("Quantity cannot be lower or equal to 0")
+        return value
+
+    def validate_dish_id(self, value: UUID):
+        try:
+            Dish.objects.get(pk=value)
+        except Dish.DoesNotExist:
+            raise serializers.ValidationError(f"Dish with PK {value} dose not exist")
+        return value
--- delivery_cub/app/custom_user/views.py
+++ delivery_cub/app/custom_user/views.py
@@ -1,13 +1,50 @@
-from rest_framework import permissions, viewsets
+from drf_spectacular.utils import extend_schema
+from rest_framework import permissions, status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from app.custom_user.models import CartPosition, CustomUser
-from app.custom_user.serializers import CartPositionSerializer, UserSerializer
+from app.custom_user.serializers import (
+    AddDishToCartSerializer,
+    CartPositionSerializer,
+    UserSerializer,
+)
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    @extend_schema(
+        request=AddDishToCartSerializer,
+        responses={status.HTTP_201_CREATED: CartPositionSerializer},
+    )
+    @action(detail=True, methods=["POST"])
+    def add_dish_to_cart(self, request, **kwargs):
+        serializer = AddDishToCartSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user: CustomUser = self.get_object()
+
+        try:
+            cart_position = CartPosition.objects.get(
+                user=user,
+                dish_id=serializer.validated_data["dish_id"],
+            )
+            cart_position.quantity += serializer.validated_data["quantity"]
+            cart_position.save()
+        except CartPosition.DoesNotExist:
+            cart_position = CartPosition.objects.create(
+                user=user,
+                dish_id=serializer.validated_data["dish_id"],
+                quantity=serializer.validated_data["quantity"],
+            )
+
+        return Response(
+            CartPositionSerializer(cart_position).data,
+            status=status.HTTP_201_CREATED,
+        )
 
 
 class CartPositionViewSet(viewsets.ModelViewSet):
